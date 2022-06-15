4748d759ee2534daf67dde97f37fe0a43d2ac04b
Gorka <93476296+Gorkabm2008@users.noreply.github.com>
2022-06-15 14:04:45 +0200

    Add files via upload

---
+++ VIEW_WIFI_PASS/README.md
@@ -0,0 +1,2 @@
+# VIEW_WIFI_PASS
+ Ver las contraseñas de tu wifi registradas en tu ordenador.
---
+++ start.py
@@ -0,0 +1,12 @@
+import os
+from unittest import result
+cmd="netsh wlan show profile"
+resultado=os.popen(cmd)
+print(resultado.read())
+miarchivo=open('wifi.csv',"a")
+red=input("ingrese el nombre de la red: ")
+cmd2="netsh wlan show profile "+red+" key=clear"
+resultado2=os.popen(cmd2)
+miarchivo.write(resultado2.read())
+
+#netsh wlan show profile Wifi 1 Planta Key=clear
