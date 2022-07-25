58c00fca28f7218f4ec6d8c0642648fb4f83ac50
A-70 <93476296+A-70@users.noreply.github.com>
2022-07-25 22:47:18 +0200

    Update start.py

--- start.py
+++ start.py
@@ -1,12 +1,17 @@
+#Designed by A-70
 import os
 from unittest import result
+
 cmd="netsh wlan show profile"
 resultado=os.popen(cmd)
+
 print(resultado.read())
+
 miarchivo=open('wifi.csv',"a")
+
 red=input("ingrese el nombre de la red: ")
+
 cmd2="netsh wlan show profile "+red+" key=clear"
 resultado2=os.popen(cmd2)
-miarchivo.write(resultado2.read())
 
-#netsh wlan show profile Wifi 1 Planta Key=clear
+miarchivo.write(resultado2.read())
